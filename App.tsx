be38b39984618a55d8566de93af36cf05d6e6721
ayegbazekie-source <ayegbazekie@gmail.com>
2026-02-04 21:26:55 +0100

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import Affiliate from './pages/Affiliate';
+import Admin from './pages/Admin';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalog" element={<Catalog />} />
+            <Route path="/affiliate" element={<Affiliate />} />
+            <Route path="/admin" element={<Admin />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
